208efe66dd78af40be361c45671c1a73b72893ac
ben <nabhanggt@gmail.com>
2026-01-03 23:41:54 +0700

    feat: add nginx proxy and automatic ssl setup

--- deploy.sh
+++ deploy.sh
@@ -40,7 +40,7 @@ if [ ! -f .env ]; then
     # Configure for Production
     sed -i 's/APP_ENV=local/APP_ENV=production/' .env
     sed -i 's/APP_DEBUG=true/APP_DEBUG=false/' .env
-    sed -i "s/APP_URL=.*/APP_URL=http:\/\/${DOMAIN_NAME}:${APP_PORT}/" .env
+    sed -i "s/APP_URL=.*/APP_URL=https:\/\/${DOMAIN_NAME}/" .env
     
     # Configure Database
     sed -i 's/DB_CONNECTION=sqlite/DB_CONNECTION=mysql/' .env
@@ -59,7 +59,73 @@ export APP_PORT=$APP_PORT
 export WWWUSER=$(id -u)
 export WWWGROUP=$(id -g)
 
-echo "🐳 Building and Starting Containers..."
+# SSL Setup
+data_path="./docker/certbot"
+rsa_key_size=4096
+domains=("$DOMAIN_NAME" "www.$DOMAIN_NAME")
+email="" # Adding a valid email is recommended
+
+if [ -d "$data_path" ]; then
+    read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+    if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+        exit
+    fi
+fi
+
+if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+    echo "### Downloading recommended TLS parameters ..."
+    mkdir -p "$data_path/conf"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+fi
+
+echo "### Creating dummy certificate for $domains ..."
+path="/etc/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live/$domains"
+docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+
+echo "### Starting nginx ..."
+docker-compose -f docker-compose.prod.yml up --force-recreate -d proxy
+
+echo "### Deleting dummy certificate for $domains ..."
+docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$domains && \
+  rm -Rf /etc/letsencrypt/archive/$domains && \
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+#Join $domains to -d args
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
+
+# Select appropriate email arg
+case "$email" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="--email $email" ;;
+esac
+
+# Enable staging mode if needed
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
+
+docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+    $staging_arg \
+    $email_arg \
+    $domain_args \
+    --rsa-key-size $rsa_key_size \
+    --agree-tos \
+    --force-renewal" certbot
+
+echo "### Reloading nginx ..."
+docker-compose -f docker-compose.prod.yml exec proxy nginx -s reload
+
+echo "🐳 Building and Starting All Containers..."
 docker-compose -f docker-compose.prod.yml up -d --build
 
 echo "🔧 Running Post-Deployment Tasks..."
