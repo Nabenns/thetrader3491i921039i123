4845f89c75f4b0d39dd40dc6c9d6bf4c7581e89b
ben <nabhanggt@gmail.com>
2026-01-13 21:53:22 +0700

    fix beneran

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'check.subscription' => \App\Http\Middleware\CheckSubscription::class,
         ]);
+        
+        // Trust all proxies (Docker/Nginx)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
